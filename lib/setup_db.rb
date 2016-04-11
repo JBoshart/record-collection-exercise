051dda540c5bbc83a203f0f40a1674106189b1a8
Jillian Boshart <Jilliliz@gmail.com>
2016-04-11 16:52:27 -0700

    Did so much.

--- lib/setup_db.rb
+++ lib/setup_db.rb
@@ -1,10 +1,55 @@
 require 'sqlite3'
+require 'CSV'
 
 class ReleaseDatabase
+  FILEPATH = "source/20160406-record-collection.csv"
+
   attr_reader :db
+
   def initialize(dbname = "releases")
     @db = SQLite3::Database.new "database/#{dbname}.db"
+  end
+
+  def reset_schema!
+    # table name: albums
+    # label_code,artist,title,label,format,released,data_added
+    # BLOG, TEXT, TEXT, TEXT, TEXT, INTEGER, TEXT
+    # one table
+    query = <<-CREATESTATEMENT
+      CREATE TABLE albums (
+        id INTEGER PRIMARY KEY,
+        label_code BLOB,
+        artist TEXT NOT NULL,
+        title TEXT NOT NULL,
+        label TEXT,
+        format TEXT,
+        released INTEGER,
+        date_added TEXT
+      );
+    CREATESTATEMENT
+
+    @db.execute("DROP TABLE IF EXISTS albums;")
+    @db.execute(query)
+  end
+
+  def load!
+    insert_statement = <<-INSERTSTATEMENT
+      INSERT INTO albums (
+      label_code, artist, title, label, format, released, date_added
+      ) VALUES (
+      :label_code, :artist, :title, :label, :format, :released, :date_added
+      );
+    INSERTSTATEMENT
+
+    prepared_statement = @db.prepare(insert_statement)
+
+    # now that we have a prepared statement, let's iterate teh csv and use its values to populate our database
+    CSV.foreach(FILEPATH, headers: true) do |row|
+      prepared_statement.execute(row.to_h)
+    end
   end
 end
 
 release_db = ReleaseDatabase.new
+release_db.reset_schema!
+release_db.load!
